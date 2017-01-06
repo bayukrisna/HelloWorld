49307b6160c7ad5d2f330813d2555fa0c11354e6
Bayu Krisna <bayu_krisna_24rpl@student.smktelkom-mlg.sch.id>
2017-01-06 07:36:06 -0800

    HelloWorld Final

---
+++ XIRPL1_08_BayuKrisna_HelloWord/Controllers/BayuController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace XIRPL1_08_BayuKrisna_HelloWord.Controllers
+{
+    public class BayuController : Controller
+    {
+        // GET: Bayu
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
